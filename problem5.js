a86c2341f81aeae5f37b98640fbc5dda5bdbf2f1
junayed17 <junayedahmednipun@gmail.com>
2026-01-08 18:30:15 +0600

    5th problem solved

---
+++ problem5.js
@@ -0,0 +1,13 @@
+// Problem 5: Remove Duplicates from an Array
+// Write a function that removes all duplicate numbers from an array.
+
+//     Example:
+
+//   Input: [1, 2, 2, 3, 4, 4]
+//   Output: [1, 2, 3, 4]
+
+function DuplicatesRemove(params) {
+  let removeDuplicates = [...new Set(params)];
+  console.log(removeDuplicates)
+}
+DuplicatesRemove( [1, 2, 2, 3, 4, 4])
